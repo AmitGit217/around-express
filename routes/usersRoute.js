e7e21f5d97e15e9b887592e8da6ae189d75b1a12
Amit Bar-Gil <amit217@yandex.com>
2022-08-08 13:08:28 +0300

    Users data from normalized path

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
-const { users } = require('../data/usersData');
+const path = require('path');
+const fs = require('fs');
+
+const usersDataFile = path.join(__dirname, '..', 'data', 'usersData.json');
 
 router.get('/users', (req, res) => {
-  res.send(users);
+  fs.readFile(usersDataFile, { encoding: 'utf8' }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(data);
+  });
 });
 
 module.exports = router;
