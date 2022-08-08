67dda923d422f9c65e44796f0d3f03fb0b05ffd2
Amit Bar-Gil <amit217@yandex.com>
2022-08-08 15:15:59 +0300

    cardRoute fs => fs.promises

--- routes/cardsRoute.js
+++ routes/cardsRoute.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs').promises;
 
 router.get('/cards', (req, res) => {
   const cardsDataFile = path.join(__dirname, '..', 'data', 'cardsData.json');
-  fs.readFile(cardsDataFile, { encoding: 'utf8' }, (err, data) => {
-    if (err) {
-      res.send(err.message);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
+  fs.readFile(cardsDataFile, { encoding: 'utf8' })
+    .then((data) => res.send(JSON.parse(data)))
+    .catch((err) => res.send(err.message));
 });
 
 module.exports = router;
