e13c627d2502a951462376a005d474f61a1f4def
Amit Bar-Gil <amit217@yandex.com>
2022-08-09 09:08:42 +0300

    Handle non exsiting route

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
+const helmet = require('helmet');
 
 const usersRoute = require('./routes/usersRoute');
 const cardsRoute = require('./routes/cardsRoute');
+const nonExistRoute = require('./routes/nonExistRoute');
 
 const app = express();
-
+app.use(helmet());
 app.use('/', usersRoute);
 app.use('/', cardsRoute);
+app.use('*', nonExistRoute);
 
 app.listen(PORT);
